6c958fdf3dac06f268a4c3565c1037d74bb62512
charlesmicheni <charlesmugambi100@gmail.com>
2020-09-28 00:24:52 +0300

    corrected pre-existig errors

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,21 +3,21 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { GoalComponent } from './goal/goal.component';
 import { QuoteFormComponent } from './quote-form/quote-form.component';
 import { QuotesComponent } from './quotes/quotes.component';
 import { QuotesDetailsComponent } from './quotes-details/quotes-details.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent,
-    GoalComponent,
     QuoteFormComponent,
     QuotesComponent,
     QuotesDetailsComponent
   ],
   imports: [
     BrowserModule,
+    FormsModule,
     AppRoutingModule
   ],
   providers: [],
